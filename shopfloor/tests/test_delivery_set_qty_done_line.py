OCA/wms

shopfloor delivery: better error when cancelled picking is scanned

shopfloor/tests/test_delivery_set_qty_done_line.py
@@ -50,7 +50,7 @@ class DeliverySetQtyDoneLineCase(DeliveryCommonCase):
         )
         self.assert_response_deliver(
             response,
-            message=self.service.msg_store.stock_picking_not_available(self.picking),
+            message=self.service.msg_store.transfer_cancelled(),
         )
 
     def test_set_qty_done_line_line_not_found(self):

shopfloor/tests/test_delivery_set_qty_done_pack.py
@@ -65,7 +65,7 @@ class DeliverySetQtyDonePackCase(DeliveryCommonCase):
         )
         self.assert_response_deliver(
             response,
-            message=self.service.msg_store.stock_picking_not_available(self.picking),
+            message=self.service.msg_store.transfer_cancelled(),
         )
 
     def test_set_qty_done_pack_package_not_found(self):

OCA/stock-logistics-shopfloor

shopfloor/tests/test_delivery_set_qty_done_line.py
@@ -52,7 +52,7 @@ class DeliverySetQtyDoneLineCase(DeliveryCommonCase):
         )
         self.assert_response_deliver(
             response,
-            message=self.service.msg_store.stock_picking_not_available(self.picking),
+            message=self.service.msg_store.transfer_canceled(),
         )
 
     def test_set_qty_done_line_line_not_found(self):

shopfloor/tests/test_delivery_set_qty_done_pack.py
@@ -67,7 +67,7 @@ class DeliverySetQtyDonePackCase(DeliveryCommonCase):
         )
         self.assert_response_deliver(
             response,
-            message=self.service.msg_store.stock_picking_not_available(self.picking),
+            message=self.service.msg_store.transfer_canceled(),
         )
 
     def test_set_qty_done_pack_package_not_found(self):