i-vyshnevska/wms

zone_picking: handle case where set line dest has no line

shopfloor_mobile/static/wms/src/scenario/zone_picking.js
@@ -133,6 +133,7 @@ const template_mobile = `
                 :message="{body: 'Full order picking, no more operation.'}"
                 />
             <picking-summary
+                v-if="stata.data.move_line"
                 :record="state.data.move_line.picking"
                 :records="[state.data.move_line]"
                 :records_grouped="picking_summary_records_grouped([state.data.move_line])"
@@ -140,12 +141,19 @@ const template_mobile = `
                 :key="make_state_component_key(['picking-summary'])"
                 />
             <item-detail-card
+                v-if="stata.data.move_line"
                 :key="make_state_component_key(['detail-move-line-dest-pack', state.data.move_line.id])"
                 :record="state.data.move_line"
                 :options="{main: true, key_title: 'package_dest.name'}"
                 :card_color="utils.colors.color_for('screen_step_todo')"
                 class="mt-2"
                 />
+            <div class="no-line-found" v-if="stata.data.move_line">
+                <!-- In theory this should not happen.
+                Handled only because something seems wrong backend side
+                and we might get here w/ no line info. -->
+                No line to process.
+            </div>
         </div>
 
         <stock-zero-check

OCA/stock-logistics-shopfloor

shopfloor_mobile/static/wms/src/scenario/zone_picking.js
@@ -133,6 +133,7 @@ const template_mobile = `
                 :message="{body: 'Full order picking, no more operation.'}"
                 />
             <picking-summary
+                v-if="state.data.move_line"
                 :record="state.data.move_line.picking"
                 :records="[state.data.move_line]"
                 :records_grouped="picking_summary_records_grouped([state.data.move_line])"
@@ -140,12 +141,19 @@ const template_mobile = `
                 :key="make_state_component_key(['picking-summary'])"
                 />
             <item-detail-card
+                v-if="state.data.move_line"
                 :key="make_state_component_key(['detail-move-line-dest-pack', state.data.move_line.id])"
                 :record="state.data.move_line"
                 :options="{main: true, key_title: 'package_dest.name'}"
                 :card_color="utils.colors.color_for('screen_step_todo')"
                 class="mt-2"
                 />
+            <div class="no-line-found" v-if="state.data.move_line">
+                <!-- In theory this should not happen.
+                Handled only because something seems wrong backend side
+                and we might get here w/ no line info. -->
+                No line to process.
+            </div>
         </div>
 
         <stock-zero-check