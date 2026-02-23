07da171bd46b1d5629274bd343a7d62398b70be2
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-02-23 16:54:35 +0100

    [IMP] shopfloor_reception: prevent user from changing an existing expiration date on a lot

--- shopfloor_reception/tests/test_multi_barcode.py
+++ shopfloor_reception/tests/test_multi_barcode.py
@@ -24,6 +24,7 @@ class TestStructuredBarcode(CommonCase):
         """
         picking = self._create_picking()
         lot = self._create_lot()
+        lot.expiration_date = None
         selected_move_line = picking.move_line_ids.filtered(
             lambda li: li.product_id == self.product_a
         )
