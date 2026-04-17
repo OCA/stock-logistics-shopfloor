60dbb8a63ada535f7ef23c4d05baaece52be1628
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-04-17 15:17:00 +0200

    [FIX] shopfloor_reception: make sure "set_lot" is skipped on existing lot scan

--- shopfloor_reception/tests/test_select_move.py
+++ shopfloor_reception/tests/test_select_move.py
@@ -131,7 +131,8 @@ class TestSelectLine(CommonCase):
         )
 
         self.assertNotEqual(
-            previous_line.id, response["data"]["set_lot"]["selected_move_line"][0]["id"]
+            previous_line.id,
+            response["data"]["set_quantity"]["selected_move_line"][0]["id"],
         )
 
     def test_scan_packaging(self):
@@ -208,8 +209,8 @@ class TestSelectLine(CommonCase):
             },
         )
         self.assertNotEqual(
-            res_u1["data"]["set_lot"]["selected_move_line"][0]["id"],
-            res_u2["data"]["set_lot"]["selected_move_line"][0]["id"],
+            res_u1["data"]["set_quantity"]["selected_move_line"][0]["id"],
+            res_u2["data"]["set_quantity"]["selected_move_line"][0]["id"],
         )
 
     def test_scan_not_tracked_product(self):
@@ -509,5 +510,5 @@ class TestSelectLine(CommonCase):
             response_b["data"]["set_lot"]["selected_move_line"][0]["lot"][
                 "expiration_date"
             ],
-            "2020-02-02T12:00:00+00:00",
+            "2020-02-02T12:00:00",
         )
