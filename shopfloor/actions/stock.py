54e6ce892f31ddbbddc8c230c8e8647b0dec005d
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-04-17 16:39:26 +0200

    [FIX] shopfloor: reset printed status when unmarking

--- shopfloor/actions/stock.py
+++ shopfloor/actions/stock.py
@@ -179,7 +179,9 @@ class StockAction(Component):
                 # still assigned
                 if split:
                     unmarked_lines = picking.move_line_ids & move_lines
-                    unmarked_lines._extract_in_split_order(default={"user_id": False})
+                    unmarked_lines._extract_in_split_order(
+                        default={"user_id": False, "printed": False}
+                    )
                 else:
                     picking.user_id = still_assigned_users[0]
 
