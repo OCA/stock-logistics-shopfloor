acsone/wms

[IMP] shopfloor: prevent exceeding qty done in location_content_transfer
Prevent exceeding qty done in location content transfer scenario in case the menu option "allow_quantity_exceeding_demand" is False

shopfloor/services/location_content_transfer.py
@@ -758,6 +758,15 @@ class LocationContentTransfer(Component):
             return self._response_for_scan_destination(
                 location, move_line, confirmation_required=barcode
             )
+        if (
+            quantity > move_line.qty_done
+            and not self.work.menu.allow_quantity_exceeding_demand
+        ):
+            return self._response_for_scan_destination(
+                location,
+                move_line,
+                message=self.msg_store.unable_to_pick_more(move_line.qty_done),
+            )
 
         self._lock_lines(move_line)
 

shopfloor/tests/test_location_content_transfer_set_destination_package_or_line.py
@@ -648,6 +648,25 @@ class LocationContentTransferSetDestinationXCase(LocationContentTransferCommonCa
         # no remaining move should exist
         self.assertEqual(picking.backorder_ids, first_done_picking)
 
+    def test_set_destination_line_exceeding_quantity(self):
+        move_line = self.picking2.move_line_ids[0]
+        self._simulate_selected_move_line(move_line)
+        # Process more than expected
+        response = self.service.dispatch(
+            "set_destination_line",
+            params={
+                "location_id": self.content_loc.id,
+                "move_line_id": move_line.id,
+                "quantity": move_line.reserved_uom_qty + 2,
+                "barcode": self.dest_location.barcode,
+            },
+        )
+        self.assert_response_scan_destination(
+            response,
+            move_line,
+            message=self.service.msg_store.unable_to_pick_more(move_line.qty_done),
+        )
+
 
 class LocationContentTransferSetDestinationXSpecialCase(
     LocationContentTransferCommonCase

OCA/stock-logistics-shopfloor

shopfloor/services/location_content_transfer.py
@@ -757,6 +757,15 @@ class LocationContentTransfer(Component):
             return self._response_for_scan_destination(
                 location, move_line, confirmation_required=barcode
             )
+        if (
+            quantity > move_line.qty_picked
+            and not self.work.menu.allow_quantity_exceeding_demand
+        ):
+            return self._response_for_scan_destination(
+                location,
+                move_line,
+                message=self.msg_store.unable_to_pick_more(move_line.qty_picked),
+            )
 
         self._lock_lines(move_line)
 

shopfloor/tests/test_location_content_transfer_set_destination_package_or_line.py
@@ -648,6 +648,25 @@ class LocationContentTransferSetDestinationXCase(LocationContentTransferCommonCa
         # no remaining move should exist
         self.assertEqual(picking.backorder_ids, first_done_picking)
 
+    def test_set_destination_line_exceeding_quantity(self):
+        move_line = self.picking2.move_line_ids[0]
+        self._simulate_selected_move_line(move_line)
+        # Process more than expected
+        response = self.service.dispatch(
+            "set_destination_line",
+            params={
+                "location_id": self.content_loc.id,
+                "move_line_id": move_line.id,
+                "quantity": move_line.quantity + 2,
+                "barcode": self.dest_location.barcode,
+            },
+        )
+        self.assert_response_scan_destination(
+            response,
+            move_line,
+            message=self.service.msg_store.unable_to_pick_more(move_line.qty_picked),
+        )
+
 
 class LocationContentTransferSetDestinationXSpecialCase(
     LocationContentTransferCommonCase