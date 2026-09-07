OCA/wms

[IMP] shopfloor_reception: Don't rely on result package to select a line
When users don't use packages for receptions, we should not rely only on its
presence on lines to not select them.

So, rely also on line progress completed at 100% (after split) to select
another one with same criteria

shopfloor_reception/services/reception.py
@@ -278,7 +278,7 @@ class Reception(Component):
         line = None
         unassigned_lines = self.env["stock.move.line"]
         for move_line in move.move_line_ids:
-            if move_line.result_package_id:
+            if move_line.progress == 100.0 or move_line.result_package_id:
                 continue
             if move_line.shopfloor_user_id.id == self.env.uid:
                 line = move_line
@@ -510,8 +510,11 @@ class Reception(Component):
     def _scan_line__by_lot(self, picking, lot):
         lines = picking.move_line_ids.filtered(
             lambda l: (
-                lot == l.lot_id
-                or (lot.name == l.lot_name and lot.product_id == l.product_id)
+                (
+                    lot == l.lot_id
+                    or (lot.name == l.lot_name and lot.product_id == l.product_id)
+                )
+                and not l.progress == 100.0
                 and not l.result_package_id
             )
         )

OCA/stock-logistics-shopfloor

shopfloor_reception/services/reception.py
@@ -336,6 +336,8 @@ class Reception(Component):
         """
         unassigned_lines = self.env["stock.move.line"]
         for line in move.move_line_ids:
+            if line.progress == 100.0:
+                continue
             if line.shopfloor_user_id.id == self.env.uid:
                 return self._scan_line__recover(picking, line, qty_done)
             elif not line.shopfloor_user_id:
@@ -594,8 +596,11 @@ class Reception(Component):
         """
         lines = picking.move_line_ids.filtered(
             lambda li: (
-                lot == li.lot_id
-                or (lot.name == li.lot_name and lot.product_id == li.product_id)
+                (
+                    lot == li.lot_id
+                    or (lot.name == li.lot_name and lot.product_id == li.product_id)
+                )
+                and not li.progress == 100.0
                 and not li.result_package_id
             )
         )