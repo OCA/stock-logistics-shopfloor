OCA/wms

[IMP] shopfloor: add _check_move_line_qty_picked()

shopfloor/services/checkout.py
@@ -845,28 +845,17 @@ class Checkout(Component):
             message = self.msg_store.record_not_found()
         for move_line in move_lines:
             qty_done = quantity_func(move_line)
-            if qty_done < 0:
-                message = {
-                    "body": _("Negative quantity not allowed."),
-                    "message_type": "error",
-                }
-            else:
-                new_line = self.env["stock.move.line"]
-                if qty_done > 0:
-                    new_line, qty_check = move_line._split_qty_to_be_done(
-                        qty_done,
-                        split_partial=False,
-                        result_package_id=False,
-                    )
+            # The move line qty is increased before the check so that the user
+            # can figure out how much extra qty. There is a check later in the
+            # process to prevent to validate such extra.
+            if qty_done >= 0:
                 move_line.qty_done = qty_done
-                if new_line:
-                    selected_line_ids.append(new_line.id)
-                if qty_done > move_line.reserved_uom_qty:
-                    return self._response_for_select_package(
-                        picking,
-                        self.env["stock.move.line"].browse(selected_line_ids).exists(),
-                        message=self.msg_store.line_scanned_qty_done_higher_than_allowed(),
-                    )
+            if message := self._check_move_line_qty_picked(move_line, qty_done):
+                return self._response_for_select_package(
+                    picking,
+                    self.env["stock.move.line"].browse(selected_line_ids).exists(),
+                    message=message,
+                )
         return self._response_for_select_package(
             picking,
             self.env["stock.move.line"].browse(selected_line_ids).exists(),

shopfloor/services/cluster_picking.py
@@ -768,13 +768,13 @@ class ClusterPicking(Component):
         if response:
             return response
 
-        new_line, qty_check = move_line._split_qty_to_be_done(quantity)
-        if qty_check == "greater":
+        if message := self._check_move_line_qty_picked(move_line, quantity):
             return self._response_for_scan_destination(
                 move_line,
-                message=self.msg_store.unable_to_pick_more(move_line.reserved_uom_qty),
+                message=message,
                 qty_done=quantity,
             )
+        new_line = move_line._split_partial_quantity_to_be_done(quantity)
 
         search = self._actions_for("search")
         bin_package = search.package_from_scan(barcode)

shopfloor/services/service.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, exceptions, fields
 from odoo.osv.expression import AND
+from odoo.tools import float_compare
 
 from odoo.addons.component.core import AbstractComponent
 
@@ -123,6 +124,16 @@ class BaseShopfloorProcess(AbstractComponent):
         message = self._check_picking_status(pickings, states=states)
         if message:
             return message
+
+    def _check_move_line_qty_picked(self, move_line, qty_picked):
+        rounding = move_line.product_id.uom_id.rounding
+        if float_compare(qty_picked, 0, precision_rounding=rounding) < 0:
+            return self.msg_store.unable_to_pick_negative()
+        if not self.work.menu.allow_quantity_exceeding_demand:
+            rounding = move_line.product_id.uom_id.rounding
+            qty_todo = move_line.reserved_uom_qty
+            if float_compare(qty_picked, qty_todo, precision_rounding=rounding) > 0:
+                return self.msg_store.unable_to_pick_more(qty_todo)
 
     def is_src_location_valid(self, location):
         """Check the source location is valid for given process.

shopfloor/tests/test_checkout_set_qty.py
@@ -195,11 +195,7 @@ class CheckoutSetCustomQtyCase(CheckoutSetQtyCommonCase):
             response,
             selected_lines,
             {line1: line1.reserved_uom_qty + 1, line2: line2.reserved_uom_qty},
-            message={
-                "body": "Please note that the scanned quantity "
-                "is higher than the maximum allowed.",
-                "message_type": "warning",
-            },
+            message=self.msg_store.unable_to_pick_more(line1.reserved_uom_qty),
         )
 
     def test_set_custom_qty_negative(self):
@@ -219,10 +215,7 @@ class CheckoutSetCustomQtyCase(CheckoutSetQtyCommonCase):
             response,
             selected_lines,
             {line1: line1.reserved_uom_qty, line2: line2.reserved_uom_qty},
-            message={
-                "body": "Negative quantity not allowed.",
-                "message_type": "error",
-            },
+            message=self.msg_store.unable_to_pick_negative(),
         )
 
     def test_set_custom_qty_partial(self):

OCA/stock-logistics-shopfloor

shopfloor/services/checkout.py
@@ -833,28 +833,17 @@ class Checkout(Component):
             message = self.msg_store.record_not_found()
         for move_line in move_lines:
             qty_picked = quantity_func(move_line)
-            if qty_picked < 0:
-                message = {
-                    "body": _("Negative quantity not allowed."),
-                    "message_type": "error",
-                }
-            else:
-                new_line = self.env["stock.move.line"]
-                if qty_picked > 0:
-                    new_line, qty_check = move_line._split_qty_to_be_done(
-                        qty_picked,
-                        split_partial=False,
-                        result_package_id=False,
-                    )
+            # The move line qty is increased before the check so that the user
+            # can figure out how much extra qty. There is a check later in the
+            # process to prevent to validate such extra.
+            if qty_picked >= 0:
                 move_line.qty_picked = qty_picked
-                if new_line:
-                    selected_line_ids.append(new_line.id)
-                if qty_picked > move_line.quantity:
-                    return self._response_for_select_package(
-                        picking,
-                        self.env["stock.move.line"].browse(selected_line_ids).exists(),
-                        message=self.msg_store.line_scanned_qty_picked_higher_than_allowed(),
-                    )
+            if message := self._check_move_line_qty_picked(move_line, qty_picked):
+                return self._response_for_select_package(
+                    picking,
+                    self.env["stock.move.line"].browse(selected_line_ids).exists(),
+                    message=message,
+                )
         return self._response_for_select_package(
             picking,
             self.env["stock.move.line"].browse(selected_line_ids).exists(),

shopfloor/services/cluster_picking.py
@@ -846,13 +846,13 @@ class ClusterPicking(Component):
         if response:
             return response
 
-        new_line, qty_check = move_line._split_qty_to_be_done(quantity)
-        if qty_check == "greater":
+        if message := self._check_move_line_qty_picked(move_line, quantity):
             return self._response_for_scan_destination(
                 move_line,
-                message=self.msg_store.unable_to_pick_more(move_line.quantity),
+                message=message,
                 qty_done=quantity,
             )
+        new_line = move_line._split_partial_quantity_to_be_picked(quantity)
 
         search = self._actions_for("search")
         bin_package = search.package_from_scan(barcode)

shopfloor/services/service.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, exceptions, fields
 from odoo.osv.expression import AND
+from odoo.tools import float_compare
 
 from odoo.addons.component.core import AbstractComponent
 
@@ -123,6 +124,16 @@ class BaseShopfloorProcess(AbstractComponent):
         message = self._check_picking_status(pickings, states=states)
         if message:
             return message
+
+    def _check_move_line_qty_picked(self, move_line, qty_picked):
+        rounding = move_line.product_id.uom_id.rounding
+        if float_compare(qty_picked, 0, precision_rounding=rounding) < 0:
+            return self.msg_store.unable_to_pick_negative()
+        if not self.work.menu.allow_quantity_exceeding_demand:
+            rounding = move_line.product_id.uom_id.rounding
+            qty_todo = move_line.quantity
+            if float_compare(qty_picked, qty_todo, precision_rounding=rounding) > 0:
+                return self.msg_store.unable_to_pick_more(qty_todo)
 
     def is_src_location_valid(self, location):
         """Check the source location is valid for given process.

shopfloor/tests/test_checkout_set_qty.py
@@ -196,11 +196,7 @@ class CheckoutSetCustomQtyCase(CheckoutSetQtyCommonCase):
             response,
             selected_lines,
             {line1: line1.quantity + 1, line2: line2.quantity},
-            message={
-                "body": "Please note that the scanned quantity "
-                "is higher than the maximum allowed.",
-                "message_type": "warning",
-            },
+            message=self.msg_store.unable_to_pick_more(line1.quantity),
         )
 
     def test_set_custom_qty_negative(self):
@@ -220,10 +216,7 @@ class CheckoutSetCustomQtyCase(CheckoutSetQtyCommonCase):
             response,
             selected_lines,
             {line1: line1.quantity, line2: line2.quantity},
-            message={
-                "body": "Negative quantity not allowed.",
-                "message_type": "error",
-            },
+            message=self.msg_store.unable_to_pick_negative(),
         )
 
     def test_set_custom_qty_partial(self):