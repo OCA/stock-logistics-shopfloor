OCA/wms

[IMP] shopfloor: validate moves
Move recent picking validation improvement in cluster picking to the
stock action validating the moves.
No moves are created by shopfloor in the cluster picking but for the
generic validation we need to consider this.
Adapt the "backorder never" test for location content transfer to set
is_shopfloor_created.

Marking a move as picked should never raise the picked quantity exceed
the need.

shopfloor/actions/stock.py
@@ -127,11 +127,11 @@ class StockAction(Component):
                 )
         for line in move_lines:
             qty_done = quantity if quantity is not None else line.reserved_uom_qty
-            line.qty_done = qty_done
             if split:
-                line._split_partial_quantity()
+                line._split_partial_quantity_to_be_done(qty_done)
             data = {
                 "shopfloor_user_id": user.id,
+                "qty_done": qty_done,
             }
             if package:
                 # destination package is set to the scanned one
@@ -192,16 +192,33 @@ class StockAction(Component):
         - moves to process are exactly the assigned moves of the related transfer:
             the transfer is validated as usual, creating a backorder.
         """
-        moves.split_unavailable_qty()
+        # remove assigned non picked moves
+        moves = moves.filtered(lambda m: not (m.state == "assigned" and not m.picked))
+
         backorders = self.env["stock.picking"]
         for picking in moves.picking_id:
+            moves_todo = picking.move_ids & moves
+            if not picking.is_shopfloor_created:
+                # Normally at this stage everything should have been fully
+                # picked but it can happen the reservation of a partially
+                # available move increases. In this case, we split the
+                # partially picked move line.
+                for ml in moves_todo.move_line_ids:
+                    ml._split_partial_quantity()
+                # Put non picked move lines in a new move.
+                for move in moves_todo:
+                    new_move = move.split_other_move_lines(
+                        move.move_line_ids.filtered(lambda ml: ml.picked)
+                    )
+                    if new_move.move_line_ids:
+                        moves_todo |= new_move
+
             # the backorder strategy is checked in the 'button_validate' method
             # on odoo standard. Since we call the sub-method '_action_done' here,
             # we have to set the context key 'cancel_backorder' as it is done
             # in the 'button_validate' method according to the backorder strategy.
             not_to_backorder = picking.picking_type_id.create_backorder == "never"
             picking = picking.with_context(cancel_backorder=not_to_backorder)
-            moves_todo = picking.move_ids & moves
             if self._check_backorder(picking, moves_todo):
                 existing_backorders = picking.backorder_ids
                 picking._action_done()

shopfloor/models/stock_move_line.py
@@ -7,8 +7,6 @@ from odoo import _, exceptions, fields, models
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_is_zero
 
-from ..exceptions import CannotProcessMoreThanPlanned
-
 _logger = logging.getLogger(__name__)
 
 
@@ -63,29 +61,9 @@ class StockMoveLine(models.Model):
         :return: the new move line if created else empty recordset
         """
         self.ensure_one()
-        rounding = self.product_uom_id.rounding
-        if float_is_zero(self.qty_done, precision_rounding=rounding):
+        if not self.picked or not self.has_quantity_reserved:
             return self.browse()
-        compare = float_compare(
-            self.qty_done, self.reserved_uom_qty, precision_rounding=rounding
-        )
-        qty_lesser = compare == -1
-        qty_greater = compare == 1
-        if qty_greater:
-            raise CannotProcessMoreThanPlanned(
-                "Quantity done cannot exceed quantity to do"
-            )
-        elif qty_lesser:
-            remaining = self.reserved_uom_qty - self.qty_done
-            new_line = self.copy({"reserved_uom_qty": remaining, "qty_done": 0})
-            # if we didn't bypass reservation update, the quant reservation
-            # would be reduced as much as the deduced quantity, which is wrong
-            # as we only moved the quantity to a new move line
-            self.with_context(
-                bypass_reservation_update=True
-            ).reserved_uom_qty = self.qty_done
-            return new_line
-        return self.browse()
+        return self._split_partial_quantity_to_be_done(self.qty_picked)
 
     def _extract_in_split_order(self, default=None):
         """Have pickings fully reserved with only those move lines.
@@ -209,11 +187,16 @@ class StockMoveLine(models.Model):
     def _split_partial_quantity_to_be_done(
         self, quantity_done, split_default_vals=None
     ):
-        """Create a new move line with the remaining quantity to process."""
+        """Create a new move line with the remaining quantity to process
+
+        :return: the new move line if created else empty recordset
+        """
         # split the move line which will be processed later (maybe the user
         # has to pick some goods from another place because the location
         # contained less items than expected)
-        remaining = self.reserved_uom_qty - quantity_done
+        remaining = max(0, self.reserved_uom_qty - quantity_done)
+        if not remaining:
+            return self.browse()
         vals = {"reserved_uom_qty": remaining, "qty_done": 0}
         if split_default_vals:
             vals.update(split_default_vals)

OCA/stock-logistics-shopfloor

shopfloor/actions/stock.py
@@ -128,11 +128,11 @@ class StockAction(Component):
                 )
         for line in move_lines:
             qty_picked = quantity if quantity is not None else line.quantity
-            line.qty_picked = qty_picked
             if split:
-                line._split_partial_quantity()
+                line._split_partial_quantity_to_be_picked(qty_done)
             data = {
                 "shopfloor_user_id": user.id,
+                "qty_picked": qty_picked,
             }
             if package:
                 # destination package is set to the scanned one
@@ -201,16 +201,33 @@ class StockAction(Component):
         - moves to process are exactly the assigned moves of the related transfer:
             the transfer is validated as usual, creating a backorder.
         """
-        moves.split_unavailable_qty()
+        # remove assigned non picked moves
+        moves = moves.filtered(lambda m: not (m.state == "assigned" and not m.picked))
+
         backorders = self.env["stock.picking"]
         for picking in moves.picking_id:
+            moves_todo = picking.move_ids & moves
+            if not picking.is_shopfloor_created:
+                # Normally at this stage everything should have been fully
+                # picked but it can happen the reservation of a partially
+                # available move increases. In this case, we split the
+                # partially picked move line.
+                for ml in moves_todo.move_line_ids:
+                    ml._split_partial_quantity()
+                # Put non picked move lines in a new move.
+                for move in moves_todo:
+                    new_move = move.split_other_move_lines(
+                        move.move_line_ids.filtered(lambda ml: ml.picked)
+                    )
+                    if new_move.move_line_ids:
+                        moves_todo |= new_move
+
             # the backorder strategy is checked in the 'button_validate' method
             # on odoo standard. Since we call the sub-method '_action_done' here,
             # we have to set the context key 'cancel_backorder' as it is done
             # in the 'button_validate' method according to the backorder strategy.
             not_to_backorder = picking.picking_type_id.create_backorder == "never"
             picking = picking.with_context(cancel_backorder=not_to_backorder)
-            moves_todo = picking.move_ids & moves
             if self._check_backorder(picking, moves_todo):
                 existing_backorders = picking.backorder_ids
                 picking._action_done()

shopfloor/models/stock_move_line.py
@@ -7,8 +7,6 @@ from odoo import exceptions, fields, models
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_is_zero
 
-from ..exceptions import CannotProcessMoreThanPlanned
-
 _logger = logging.getLogger(__name__)
 
 
@@ -48,29 +46,9 @@ class StockMoveLine(models.Model):
         :return: the new move line if created else empty recordset
         """
         self.ensure_one()
-        rounding = self.product_uom_id.rounding
-        if float_is_zero(self.qty_picked, precision_rounding=rounding):
+        if not self.picked or not self.has_quantity_reserved:
             return self.browse()
-        compare = float_compare(
-            self.qty_picked, self.quantity, precision_rounding=rounding
-        )
-        qty_lesser = compare == -1
-        qty_greater = compare == 1
-        if qty_greater:
-            raise CannotProcessMoreThanPlanned(
-                "Quantity done cannot exceed quantity to do"
-            )
-        elif qty_lesser:
-            remaining = self.quantity - self.qty_picked
-            new_line = self.copy(
-                {"quantity": remaining, "qty_picked": 0, "picked": False}
-            )
-            # if we didn't bypass reservation update, the quant reservation
-            # would be reduced as much as the deduced quantity, which is wrong
-            # as we only moved the quantity to a new move line
-            self.quantity = self.qty_picked
-            return new_line
-        return self.browse()
+        return self._split_partial_quantity_to_be_picked(self.qty_picked)
 
     def _extract_in_split_order(self, default=None):
         """Have pickings fully reserved with only those move lines.
@@ -137,14 +115,17 @@ class StockMoveLine(models.Model):
             return (new_line, "lesser")
         return (new_line, "full")
 
-    def _split_partial_quantity_to_be_picked(
-        self, quantity_done, split_default_vals=None
-    ):
-        """Create a new move line with the remaining quantity to process."""
+    def _split_partial_quantity_to_be_picked(self, quantity_done, split_default_vals=None):
+        """Create a new move line with the remaining quantity to process
+
+        :return: the new move line if created else empty recordset
+        """
         # split the move line which will be processed later (maybe the user
         # has to pick some goods from another place because the location
         # contained less items than expected)
-        remaining = self.quantity - quantity_done
+        remaining = max(0, self.quantity - quantity_done)
+        if not remaining:
+            return self.browse()
         vals = {"quantity": remaining, "picked": False, "qty_picked": 0}
         if split_default_vals:
             vals.update(split_default_vals)