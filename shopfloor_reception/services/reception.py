OCA/wms

[FIX] shopfloor_reception: restrict line selection to "current user" or "no user".
A condition on the lines filtered lines has been added so to ensure that 2 users working on the same move do not steal move lines from one another.

shopfloor_reception/services/reception.py
@@ -551,6 +551,7 @@ class Reception(Component):
                     or (lot.name == l.lot_name and lot.product_id == l.product_id)
                 )
                 and not l.shopfloor_unloaded
+                and l.shopfloor_user_id.id in (False, self.env.uid)
             )
         )
         if not lines:
@@ -572,7 +573,9 @@ class Reception(Component):
     def _scan_line__fallback(self, picking, barcode):
         # We might have lines with no lot, but with a lot_name.
         lines = picking.move_line_ids.filtered(
-            lambda l: l.lot_name == barcode and not l.shopfloor_unloaded
+            lambda l: l.lot_name == barcode
+            and not l.shopfloor_unloaded
+            and l.shopfloor_user_id.id in (False, self.env.uid)
         )
         if not lines:
             return self._response_for_select_move(

OCA/stock-logistics-shopfloor

shopfloor_reception/services/reception.py
@@ -601,6 +601,7 @@ class Reception(Component):
                     or (lot.name == li.lot_name and lot.product_id == li.product_id)
                 )
                 and not li.shopfloor_unloaded
+                and li.shopfloor_user_id.id in (False, self.env.uid)
             )
         )
         if not lines:
@@ -622,7 +623,9 @@ class Reception(Component):
     def _scan_line__fallback(self, picking, barcode):
         # We might have lines with no lot, but with a lot_name.
         lines = picking.move_line_ids.filtered(
-            lambda li: li.lot_name == barcode and not li.shopfloor_unloaded
+            lambda li: li.lot_name == barcode
+            and not li.shopfloor_unloaded
+            and li.shopfloor_user_id.id in (False, self.env.uid)
         )
         if not lines:
             return self._response_for_select_move(