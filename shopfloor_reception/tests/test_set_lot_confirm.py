acsone/wms

[FIX] shopfloor_reception: enforce lot creation restriction
Enforce the 'use_create_lots' setting from the picking type during the
reception process.

shopfloor_reception/tests/test_set_lot_confirm.py
@@ -41,6 +41,7 @@ class TestSetLotConfirm(CommonCase):
 
     def test_set_new_lot(self):
         picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = True
         selected_move_line = picking.move_line_ids.filtered(
             lambda l: l.product_id == self.product_a
         )
@@ -171,6 +172,7 @@ class TestSetLotConfirm(CommonCase):
     @freeze_time("2020-01-01 11:00:00")
     def test_set_new_lot_and_expiration_date(self):
         picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = True
         selected_move_line = picking.move_line_ids.filtered(
             lambda l: l.product_id == self.product_a
         )
@@ -237,3 +239,41 @@ class TestSetLotConfirm(CommonCase):
             0,
             "No new lot should have been created in case of error.",
         )
+
+    def test_set_new_lot_creation_disabled_error(self):
+        picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = False
+
+        selected_move_line = picking.move_line_ids.filtered(
+            lambda l: l.product_id == self.product_a
+        )
+        selected_move_line.shopfloor_user_id = self.env.uid
+
+        lot_name = "NewForbiddenLot"
+
+        nb_lots_before = self.env["stock.lot"].search_count([("name", "=", lot_name)])
+        response = self.service.dispatch(
+            "set_lot_confirm_action",
+            params={
+                "picking_id": picking.id,
+                "selected_line_id": selected_move_line.id,
+                "lot_name": lot_name,
+            },
+        )
+
+        # The response should keep us on 'set_lot' and show the error message
+        expected_selected_move_line_data = self.data.move_lines(selected_move_line)
+        expected_selected_move_line_data[0]["lot"] = {"name": lot_name}
+
+        self.assert_response(
+            response,
+            next_state="set_lot",
+            data={
+                "picking": self.data.picking(picking),
+                "selected_move_line": expected_selected_move_line_data,
+            },
+            message=self.msg_store.lot_creation_disabled(picking.picking_type_id),
+        )
+
+        nb_lots_after = self.env["stock.lot"].search_count([("name", "=", lot_name)])
+        self.assertEqual(nb_lots_after, nb_lots_before)

OCA/stock-logistics-shopfloor

shopfloor_reception/tests/test_set_lot_confirm.py
@@ -41,6 +41,7 @@ class TestSetLotConfirm(CommonCase):
 
     def test_set_new_lot(self):
         picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = True
         selected_move_line = picking.move_line_ids.filtered(
             lambda li: li.product_id == self.product_a
         )
@@ -171,6 +172,7 @@ class TestSetLotConfirm(CommonCase):
     @freeze_time("2020-01-01 11:00:00")
     def test_set_new_lot_and_expiration_date(self):
         picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = True
         selected_move_line = picking.move_line_ids.filtered(
             lambda li: li.product_id == self.product_a
         )
@@ -237,3 +239,41 @@ class TestSetLotConfirm(CommonCase):
             0,
             "No new lot should have been created in case of error.",
         )
+
+    def test_set_new_lot_creation_disabled_error(self):
+        picking = self._create_picking()
+        picking.picking_type_id.sudo().use_create_lots = False
+
+        selected_move_line = picking.move_line_ids.filtered(
+            lambda li: li.product_id == self.product_a
+        )
+        selected_move_line.shopfloor_user_id = self.env.uid
+
+        lot_name = "NewForbiddenLot"
+
+        nb_lots_before = self.env["stock.lot"].search_count([("name", "=", lot_name)])
+        response = self.service.dispatch(
+            "set_lot_confirm_action",
+            params={
+                "picking_id": picking.id,
+                "selected_line_id": selected_move_line.id,
+                "lot_name": lot_name,
+            },
+        )
+
+        # The response should keep us on 'set_lot' and show the error message
+        expected_selected_move_line_data = self.data.move_lines(selected_move_line)
+        expected_selected_move_line_data[0]["lot"] = {"name": lot_name}
+
+        self.assert_response(
+            response,
+            next_state="set_lot",
+            data={
+                "picking": self.data.picking(picking),
+                "selected_move_line": expected_selected_move_line_data,
+            },
+            message=self.msg_store.lot_creation_disabled(picking.picking_type_id),
+        )
+
+        nb_lots_after = self.env["stock.lot"].search_count([("name", "=", lot_name)])
+        self.assertEqual(nb_lots_after, nb_lots_before)